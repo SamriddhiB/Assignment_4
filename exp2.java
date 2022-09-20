a92f3bf4a3cb81864973f2be285e6ccfd882a08b
SamriddhiB <110332382+SamriddhiB@users.noreply.github.com>
2022-09-20 21:03:43 +0530

    Update exp2.java

--- exp2.java
+++ exp2.java
@@ -1,3 +1,5 @@
+/*Write a program that raises two exceptions. Specify two ‘catch’clauses for the two exceptions. Each ‘catch’ block handles a different type of exception. For example the exception could be
+‘ArithmeticException’ and ‘ArrayIndexOutOfBoundsException’. Display a message in the ‘finally’ block.*/
 import java.util.Scanner;
 
 public class exp2 {
